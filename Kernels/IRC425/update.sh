8771e52b594fdb12dfea5de74259f6fda3a865f7
hdpriest <hdpriest@illinois.edu>
2024-12-16 14:10:37 -0600

    intermediate commit; sync to cc

--- Kernels/IRC425/update.sh
+++ Kernels/IRC425/update.sh
@@ -4,37 +4,40 @@
 
 # requiring the kernel name to also be the directory name as well as in the tags isn't the most robust method
 custom_kernel="IRC425"
-custom_kernel_path="./Kernels/" $custom_kernel "/"
-
-sed -i~ -e '/^## Current Release/,$d' README.md
-echo "## Current Release" >> README.md
-echo "" >> README.md
-echo "This is the list of releases for $custom_kernel $(jq -r ."[$custom_kernel]" tags.json)" >> README.md
-echo "" >> README.md
-echo "| module | version |" >> README.md
-echo "| ------ | ------- |" >> README.md
+custom_kernel_path="./Kernels/$custom_kernel/"
+json_tags="$custom_kernel_path""tags.json"
+#sed -i~ -e '/^## Current Release/,$d' README.md
+#echo "## Current Release" >> README.md
+#echo "" >> README.md
+#echo "This is the list of releases for $custom_kernel $(jq -r ."[$custom_kernel]" tags.json)" >> README.md
+#echo "" >> README.md
+#echo "| module | version |" >> README.md
+#echo "| ------ | ------- |" >> README.md
 
 # TODO: this will update all submodules in the repo - not just for this custom kernel
 git submodule update --remote
-for dir in $(jq -r 'keys[]' custom_kernel_path"/"tags.json | sort); do
+#for dir in $(jq -r 'keys[]' $custom_kernel_path"/"tags.json | sort); do
+for dir in $(jq -r 'keys[]' $json_tags | sort); do
   if [ "$dir" == "$custom_kernel" ]; then continue; fi
-  tag=$(jq -r ".\"$dir\"" tags.json)
+  #tag=$(jq -r ".\"$dir\"" tags.json)
+  tag=$(jq -r ".\"$dir\"" $json_tags)
   if [ "$tag" == "null" ]; then
     echo "[$dir] missing tag"
   else
     if [ ! -d $dir ]; then
       echo "[$dir] missing submodule"
-      git submodule add https://github.com/IN-CORE/$dir $dir
+#      git submodule add https://github.com/IN-CORE/$dir $dir
     fi
     echo "[$dir] checking out $tag"
-    sed -i~ "s/^| $dir | .* |$/| $dir | $tag |/" README.md
-    rm README.md~
-    (cd $dir; git checkout $tag)
-    git add $dir
+#    sed -i~ "s/^| $dir | .* |$/| $dir | $tag |/" README.md
+#    rm README.md~
+#    (cd $dir; git checkout $tag)
+#    git add $dir
   fi
-  echo "| ${dir} | ${tag} |" >> README.md
+  echo "| ${dir} | ${tag} |"
+#  echo "| ${dir} | ${tag} |" >> README.md
 done
 
-git add README.md tags.json
-git submodule status
-git status
+#git add README.md tags.json
+#git submodule status
+#git status
